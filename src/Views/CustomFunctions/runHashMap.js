23611e71eb6bc7bb1c7a614a3e44472f4b2d7fbb
linhdoan9513 <78831085+linhdoan9513@users.noreply.github.com>
2021-10-06 19:46:46 -0400

    Clean up runHashMap

--- src/Views/CustomFunctions/runHashMap.js
+++ src/Views/CustomFunctions/runHashMap.js
@@ -1,30 +1,22 @@
 //Create a higher level function to create an object that store the total amount of each category from the database of category to display in Income by Category section
 // Implement using HashMap algorithms;
 
-const runHashMap = (dbCategory, defaultCategory) => {
-  const categoryTypes = defaultCategory;
+const runHashMap = (dbCategories, categoryTypes) => {
   // Create an empty object to store all total amount of each categories from the userInput and match with default categories list (defaultCategory)
   let categoryObject = {};
 
   //Create a loop to store the name of default category in object and set the initial value of total amount as 0;
-  const categoryTypeMap = () => {
-    for (let i = 0; i < categoryTypes.length; i++) {
-      categoryObject[categoryTypes[i]] = 0;
-    }
-    return categoryObject;
-  };
-  categoryTypeMap();
+  categoryTypes.forEach((category) => {
+    categoryObject[category] = 0;
+  });
 
   //Function to match all userInput in database with default categories, and add the total amount in the correct category.
-  const categoryAmountMap = () => {
-    for (let i = 0; i < dbCategory.length; i++) {
-      if (dbCategory[i][1].category in categoryObject) {
-        categoryObject[dbCategory[i][1].category] += dbCategory[i][1].amount;
-      }
+  dbCategories.forEach((dbCategory) => {
+    if (dbCategory[1].category in categoryObject) {
+      categoryObject[dbCategory[1].category] += dbCategory[1].amount;
     }
-  };
+  });
 
-  categoryAmountMap();
   return categoryObject;
 };
 
